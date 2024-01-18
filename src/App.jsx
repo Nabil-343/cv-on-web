441d0fcc5a0cdbe96a44a8eddccad522a5f857bb
Nabil-343 <verdict.final@gmail.com>
2024-01-18 22:05:33 +0500

    init basic app

---
+++ src/App.jsx
@@ -0,0 +1,73 @@
+
+
+function App() {
+  
+  return (
+   <div class= "container"> 
+   
+   <div className="row1"></div>
+    <div className= "row" >
+      <div className="col-3" id="info" >
+        <h2>Nabil Hassan</h2>
+        <ul>
+          <li><span>Birthday:</span> (+92) 3435926579</li>
+          <li><span>Phone number:</span> (+92) 3435926579</li>
+          <li><span>Email address:</span> nabil.1984@yahoo.com</li>
+          <li><span>Address:</span> Rawalpindi </li>
+        </ul>
+      </div>
+     
+      <div className="col-9" id="content">
+        <h2>ABOUT ME</h2>
+        <p>I work full-time in the public sector where I oversee a team of senior auditors
+           and data entry operators in a supervisory role i.e. Asst. Accounts Officer. My main responsibility is managing
+           and ensuring the efficiency of daily operations related to Audit and Accounts.
+           Despite my current career, my true passion lies in the tech industry, particularly
+           in development.I find joy in coding and working on projects using technologies like
+           React and Node.js.
+          </p>
+        
+        <h2>WORK EXPERIENCE</h2>
+          <ul>
+            <li>
+              <h3>MERN Stack Developer</h3>
+              <p>Working as a Mern Stack developer as a freelancer</p>
+            </li>
+             <li>
+              <h3>Asst. Accounts Officer</h3>
+              <p>Supervision and managing a team of data entry operators, Junior auditors,
+               and senior auditors, ensuring their efficient performance and adherence to
+               accounting principles and accurate financial data entries.</p>
+              </li>
+            <li>
+              <h3>Senior Auditor</h3>
+              <p>Conducting financial audits for General provident Fund and Pensions,
+                ensuring compliance with regulations through data analysis.</p>
+              </li>
+          </ul>
+         <h2>Education</h2>
+          <ul>
+            <li>
+              <h3>MERN Stack Developer Course</h3>
+              <p>NexusBerry Institute</p>
+              </li>
+            <li>
+              <h3>PIPFA Affiliate Certificate</h3>
+              <p>Pakistan Institute of Public Finance Accountants</p>
+              </li>
+            <li>
+              <h3>Masters of Business Administration</h3>
+              <p>AIOU</p>
+              </li> 
+          </ul>
+                      
+      </div>
+      
+    </div>
+    <div className= "row3" ></div>
+   
+   </div>
+  )
+}
+
+export default App
